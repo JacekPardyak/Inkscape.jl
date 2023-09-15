2f0092989f31001a21a6795a7f4b54c3630182cb
JacekPardyak <jacek.pardyak@gmail.com>
2023-09-15 22:08:23 +0200

    cmt

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Inkscape
 using Test
 
 @testset "Inkscape.jl" begin
-    @test Inkscape.inx_version() == "Hello Inkscape!"
+    @test Inkscape.inx_install() != "Hello Inkscape!"
+    @test Inkscape.inx_version() != "Hello Inkscape!"
     @test Inkscape.inx_version() != "Hello world!"
 end
